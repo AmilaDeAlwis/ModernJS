a3aa09f9b9195c56490e5bad1d67e4b1e58b9e04
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-04-26 18:38:12 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+//value changeble variables
+let age1 = 25;
+let year1 = 2022;
+
+console.log(age1, year1);
+
+let age2 = 20;
+let year2 = 2021;
+
+console.log(age2, year2);
+year2 = 2030; //year changed
+console.log(year2);
+
+//value can't change later
+const points = 100;
+console.log(points);
