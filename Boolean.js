3220d942d92a6e69d19b2bbf107c64340e097715
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-04-26 20:47:32 +0530

    Boolean

---
+++ Boolean.js
@@ -0,0 +1,28 @@
+console.log(true, false, 'true', 'false');
+
+let email = 'luigi@gmail.com';
+let names = ['mario', 'luigi', 'shaun'];
+
+let result1 = email.includes('c');
+let result2 = names.includes('wario');
+
+console.log(result1, result2);
+
+//boolean operators with strings
+let person = 'shaun';
+
+console.log(person == 'shaun');
+console.log(person == 'Shaun');
+console.log(person != 'crystal');
+console.log(person > 'Shaun');
+
+//boolean operators with numbers
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age >=25);
+console.log(age <=25);
